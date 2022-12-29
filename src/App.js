929953485d358d046ba7bafa555aa5c8fb86864b
Jing Hong Gang <honggangjing@gmail.com>
2022-12-29 11:45:19 +0800

    feat: 08｜组件生命周期：React新老版本中生命周期的演化

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 const KanbanCard = ({title, status}) => {
+  const [displayTime, setDisplayTime] = useState(status);
+  useEffect(() => {
+    const updateDisplayTime = () => {
+      const timePassed = new Date() - new Date(status)
+      let relativeTime = '刚刚'
+      if (timePassed >= 60 * 1000 && timePassed < 60 * 60 * 1000) {
+        relativeTime = `${Math.ceil(timePassed / (60 * 1000))} 分钟前`
+      } else if (timePassed >= 60 * 60 * 1000 && timePassed < 60 * 60 * 1000 * 24) {
+        relativeTime = `${Math.ceil(timePassed / (60 * 60 * 1000))} 小时前`
+      } else if (timePassed >= 60 * 60 * 1000 * 24) {
+        relativeTime  = `${Math.ceil(timePassed / (60 * 60 * 1000 * 24))} 天前`
+      }
+      setDisplayTime(relativeTime)
+    }
+    const intervalId = setInterval(updateDisplayTime, 60 * 1000)
+    updateDisplayTime()
+    return () => {
+      clearInterval(intervalId)
+    };
+  }, [status]);
+
   return (
     <li className="kanban-card">
       <div className="card-title">{title}</div>
-      <div className="card-status">{status}</div>
+      <div className="card-status">{displayTime}</div>
     </li>
   )
 }
@@ -34,17 +55,19 @@ const KanbanNewCard = ({onSubmit}) => {
 function KanbanBoard() {
   const [showAdd, setShowAdd] = useState(false);
   const [todoList, setTodoList] = useState([
-    {title: '开发任务-1', status: '22-05-22 18:15'},
-    {title: '开发任务-3', status: '22-05-22 18:15'},
-    {title: '开发任务-5', status: '22-05-22 18:15'},
-    {title: '测试任务-3', status: '22-05-22 18:15'}]);
+    {title: '开发任务-1', status: '2022-05-22 18:15'},
+    {title: '开发任务-3', status: '2022-05-22 18:15'},
+    {title: '开发任务-5', status: '2022-05-22 18:15'},
+    {title: '测试任务-3', status: '2022-05-22 18:15'}]);
+  // eslint-disable-next-line no-unused-vars
   const [ongoingList, setOngoingList] = useState([
-    {title: '开发任务-4', status: '22-05-22 18:15'},
-    {title: '开发任务-6', status: '22-05-22 18:15'},
-    {title: '测试任务-2', status: '22-05-22 18:15'}]);
+    {title: '开发任务-4', status: '2022-05-22 18:15'},
+    {title: '开发任务-6', status: '2022-05-22 18:15'},
+    {title: '测试任务-2', status: '2022-05-22 18:15'}]);
+  // eslint-disable-next-line no-unused-vars
   const [doneList, setDoneList] = useState([
-    {title: '开发任务-2', status: '22-05-22 18:15'},
-    {title: '测试任务-1', status: '22-05-22 18:15'}]);
+    {title: '开发任务-2', status: '2022-05-22 18:15'},
+    {title: '测试任务-1', status: '2022-05-22 18:15'}]);
 
   const handleAdd = () => {
     setShowAdd(true)
